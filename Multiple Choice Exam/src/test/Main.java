577765d2c812976cb610ab15362cdc01ca59b4f3
elhaj <sidimed03@gmail.com>
2015-11-27 16:34:11 -0600

    Database Definition file

--- Multiple Choice Exam/src/test/Main.java
+++ Multiple Choice Exam/src/test/Main.java
@@ -1,14 +1,15 @@
 package test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 
 		HibernateExporter exporter = new HibernateExporter("org.hibernate.dialect.MySQL5Dialect", "model");
 		exporter.setGenerateDropQueries(true);
-		exporter.exportToConsole();
+		exporter.export(new File("C:\\Users\\HP\\git\\LocalOnlineExamRepository\\Multiple Choice Exam\\src\\resources\\DD.txt"));
 	}
-
 }
